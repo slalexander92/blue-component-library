49a7b15d8bc7ef5a5c0ced0686cb079b009cd9ca
Spencer Alexander <salexander@bluemodus.com>
2024-02-26 17:14:04 -0800

    chore(config): set up `package.json`, `stencil.config.ts`, `tsconfig.json`

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,22 +1,25 @@
 import { Config } from '@stencil/core';
+import { sass } from '@stencil/sass';
 
 export const config: Config = {
   namespace: 'blue-component-library',
-  outputTargets: [
-    {
-      type: 'dist',
-      esmLoaderPath: '../loader',
-    },
-    {
-      type: 'dist-custom-elements',
-    },
-    {
-      type: 'docs-readme',
-    },
-    {
-      type: 'www',
-      serviceWorker: null, // disable service workers
-    },
+  outputTargets: [{
+    type: 'dist',
+    esmLoaderPath: '../loader',
+  }, {
+    type: 'dist-custom-elements',
+  }, {
+    type: 'docs-readme',
+  }, {
+    type: 'www',
+    serviceWorker: null, // disable service workers
+  }],
+  plugins: [
+    sass({
+      injectGlobalPaths: [
+        'src/styles/global.scss',
+      ],
+    }),
   ],
   testing: {
     browserHeadless: "new",
